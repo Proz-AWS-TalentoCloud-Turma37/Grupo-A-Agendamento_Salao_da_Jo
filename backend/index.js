20295b19f85f883176e59bac6eb62b6d633c40a3
igor alves <igoralvespalmeirag4@gmail.com>
2025-01-14 15:39:42 -0300

    feito controladores, e rotas de serviços e categorias.

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import usuarioRoutes from "./src/routes/usuarioRoutes.js";
 import horarioRoutes from "./src/routes/horarioRoutes.js";
 import agendamentoRoutes from "./src/routes/agendamentoRoutes.js";
 import servicoRoutes from "./src/routes/servicoRoutes.js"; // Importa as rotas de serviços
+import categoriaRoutes from "./src/routes/categoriaRoutes.js"; // Importa as rotas de categorias
 
 const app = express();
 const PORT = env.PORT || 3000; // Usa a porta do arquivo .env ou 3000 como padrão
@@ -17,6 +18,7 @@ app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/horarios", horarioRoutes);
 app.use("/api/agendamentos", agendamentoRoutes);
 app.use("/api/servicos", servicoRoutes); // Adiciona a rota de serviços
+app.use("/api/categorias", categoriaRoutes); // Adiciona a rota de categorias
 
 // Middleware de erro
 app.use((err, req, res, next) => {
